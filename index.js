7e4a8b7427e78344829848cfc84ff3010ca670c4
Your Name <you@example.com>
2022-03-19 18:48:42 +0300

    0.00.03

--- index.js
+++ index.js
@@ -24,37 +24,20 @@ app.use(express.json());
             "status" : status ,
             "token" : token 
         });
-    }) ;
+    });
 }); 
 
 
 
 app.post('/login', (req, res) => {
-    var qlTe = 'SELECT * FROM clients WHERE phone = ?';
-    db.query(qlTe, [req.body.phone], function (err, result) {
-        if (err) throw err;
-        const { username, name, phone, password } = result[0];
-        bcrypt.compare(req.body.password, password, function (err, result) {
-            if (result) {
-                const token = jwt.sign({ username, name, phone }, process.env.JWT_KEYWORD, {
-                    algorithm: "HS256",
-                    expiresIn: jwtExpirySeconds,
-                })
-                res.status(200).json({
-                    status: 'success',
-                    token,
-                });
-
-            } else {
-                res.status(403).json({
-                    status: 'username or password is wrong',
-                });
-            }
+    const loginPage = require('./src/routes/auth/login');
+    loginPage.login(req.body , function (result){
+        const {status , token , code} = result ;
+        res.status(code).json({
+            "status" : status ,
+            "token" : token 
         });
     });
-
-
-
 });
 
 
--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -1,30 +1,64 @@
-function login(body){
+const bcrypt = require('bcrypt');
+const configDb = require('../../configs/dataBaseConfigs');
+const jwt = require('jsonwebtoken');
 
 
-    var qlTe = 'SELECT * FROM clients WHERE phone = ?';
+const jwtExpirySeconds = 4320000;
+const qlTe = 'SELECT * FROM clients WHERE phone = ?';
+var db = configDb.db;
 
-    db.query(qlTe, [body.phone], function (err, result) {
-        if (err) throw err;
+
+
+function login(body, callback) {
+    const bodyPhone = body.phone;
+    const bodyPassword = body.password;
+
+    db.query(qlTe, [bodyPhone], function (err, result) {
+        if (err) return callback({
+            "status": "failed",
+            "token": "null",
+            "message" : "Server Error",
+            code: 500
+        });
+        if (result == 0) {
+            return callback({
+                "status": "failed",
+                "token": "null",
+                "message" : "User not exist",
+                code: 403
+            });
+        }
         const { username, name, phone, password } = result[0];
-        bcrypt.compare(body.password, password, function (err, result) {
+        bcrypt.compare(bodyPassword, password, async function (err, result) {
             if (result) {
-                const token = jwt.sign({ username, name, phone }, process.env.JWT_KEYWORD, {
-                    algorithm: "HS256",
-                    expiresIn: jwtExpirySeconds,
-                })
-                res.status(200).json({
-                    status: 'success',
-                    token,
+                var token = await getToken(username, name, phone);
+                return callback({
+                    "status": "succses",
+                    "token": token,
+                    "message" : "loged in",
+                    code: 200
                 });
-
             } else {
-                res.status(403).json({
-                    status: 'username or password is wrong',
+                return callback({
+                    "status": "failed",
+                    "token": "null",
+                    "message" : "Wrong Password",
+                    code: 403
                 });
             }
         });
+
     });
-
-
 }
 
+function getToken(username, name, phone) {
+    const token = jwt.sign({ username, name, phone }, process.env.JWT_KEYWORD, {
+        algorithm: "HS256",
+        expiresIn: jwtExpirySeconds,
+    });
+    return token;
+}
+
+
+
+module.exports.login = login;
